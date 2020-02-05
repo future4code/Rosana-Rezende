3c76fafa8f20536ec09427f965a723c465ecba9b
Rosana Rezende <contato@rosanarezende.com>
2020-02-05 17:29:50 -0300

    Desafio 1 - acrescentando a parte do campo imagem ser opcional

--- semana4/classes/blog/index.js
+++ semana4/classes/blog/index.js
@@ -16,32 +16,33 @@ const criarPost = () => {
     let novoImagem = document.getElementById("novo-imagem").value
     let novoConteudo = document.getElementById("novo-conteudo").value
     let novoHashtags = document.getElementById("novo-hashtags").value
-    let publicacao = new Post(novoTitulo, novoAutor, novoImagem, novoConteudo, novoHashtags)
-    arrayDePosts.push(publicacao)
     
-    //TESTE
-    // console.log(publicacao)
+    if ((novoTitulo === "") || (novoAutor === "") 
+    || (novoConteudo === "") || (novoHashtags === "")) {
+        alert("Preencha todos os campos obrigatórios [Título / Autor / Conteúdo / HashTags]")
+    } else {
+        // CONSOLE
+        let publicacao = new Post(novoTitulo, novoAutor, novoImagem, novoConteudo, novoHashtags)
+        arrayDePosts.push(publicacao)
 
-    // LIMPAR
-    document.getElementById("novo-titulo").value = ""
-    document.getElementById("novo-autor").value = ""
-    document.getElementById("novo-imagem").value = ""
-    document.getElementById("novo-conteudo").value = ""
-    document.getElementById("novo-hashtags").value = ""
-    console.log(arrayDePosts)
-
-    // // TESTES
-    // console.log(arrayDePosts[0].titulo)
-
-    colocarPostNaPagina(novoTitulo, novoAutor, novoImagem, novoConteudo, novoHashtags)
-
+        // POSTAGEM
+        colocarPostNaPagina(novoTitulo, novoAutor, novoImagem, novoConteudo, novoHashtags)
+    
+        // LIMPAR
+        document.getElementById("novo-titulo").value = ""
+        document.getElementById("novo-autor").value = ""
+        document.getElementById("novo-imagem").value = ""
+        document.getElementById("novo-conteudo").value = ""
+        document.getElementById("novo-hashtags").value = ""
+        console.log(arrayDePosts)            
+    }    
 }
 
 function colocarPostNaPagina(novoTitulo, novoAutor, novoImagem, novoConteudo, novoHashtags) {
     document.getElementById("main-container").innerHTML += 
         "<article><h3>" + novoTitulo + 
-        "</h3><h6>" + novoAutor + 
-        "</h6><img src=\'" + novoImagem + 
+        "</h3><p>Escrito por: <b><i>" + novoAutor + 
+        "</i></b></p><img src=\'" + novoImagem + 
         "\'><p>" + novoConteudo + 
         "</p><p>" + novoHashtags + "</p></article>"
 }
