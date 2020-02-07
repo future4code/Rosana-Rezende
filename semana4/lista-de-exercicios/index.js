0db4cf6e5a0511ac2e7b4ba22aae3e27394fd028
Rosana Rezende <contato@rosanarezende.com>
2020-02-07 10:58:46 -0300

    2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -13,6 +13,30 @@ Por fim, impreme-se o valor da variável meuDinheiro no console.
 O valor impresso será: R$ com 100 multiplicado pela cotação que o usuário informar
     ex: se cotacao = 10, imprime-se R$1000
 
+
+2)
+A função investeDinheiro recebe 2 parâmetro: tipoDeInvestimento e valor.
+Dentro da função, declara-se a variável valorAposInvestimento.
+Em seguida usa-se a condicional switch case para determinar o que o parâmetro 
+tipoDeInvestimento irá retornar, por exemplo: se o tipoDeInvestimento for "Poupança", 
+irá retornar valorAposInvestimento = valor passado como parâmetro multiplicado por 1.03.
+E caso o tipoDeInvestimento não seja encontrado um alert informará ao usuário que o 
+investimento informado está incorreto.
+Ao final, a função retornará o valorAposInvestimento.
+
+Para que essa função seja chamada há um callback.
+A variável novoMontante recebeu a função investeDinheiro com os parâmetros: 
+a string "Ações" e o number 150.
+A variável segundoMontante recebeu a função investeDinheiro com os parâmetros: 
+a string "Tesouro Direto" e o number 200.
+
+Imprime-se a variável novoMontante no console. 
+Resultado: aparece 165 no console
+
+Imprime-se a variável segundoMontante no console. Resultado:
+abre-se um alert com a mensagem "TIPO DE INVESTIMENTO INFORMADO INCORRETO!"
+aparece undefined no console
+
 */
 
 // --------------------- EXERCÍCIOS DE LÓGICA DE PROGRAMAÇÃO
