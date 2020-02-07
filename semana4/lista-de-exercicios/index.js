fe8dad6783b4fc1a6a9d5197ba16cc9b3ebccd08
Rosana Rezende <contato@rosanarezende.com>
2020-02-07 10:57:39 -0300

    EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO - 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,39 @@
+// --------------------- EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+/*
+
+1)
+A função conversorDeMoeda recebe o parâmetro valoeEmDolar.
+Essa função pede ao usuário que informe o valor da cotação do dólar, que será convertido para número.
+Em seguida, retorna uma string concatenando o cifrão de real com o valor em dolar (parâmetro recebido pela função) multiplicado pela coração informada pelo usuário.
+
+Para que essa função seja chamada há um callback.
+A variável meuDinheiro recebeu a função conversorDeMoeda com o parâmetro number 100.
+
+Por fim, impreme-se o valor da variável meuDinheiro no console.
+O valor impresso será: R$ com 100 multiplicado pela cotação que o usuário informar
+    ex: se cotacao = 10, imprime-se R$1000
+
+*/
+
+// --------------------- EXERCÍCIOS DE LÓGICA DE PROGRAMAÇÃO
+
+
+
+
+
+
+
+
+// --------------------- EXERCÍCIOS DE FUNÇÕES
+
+
+
+
+// --------------------- EXERCÍCIOS DE OBJETOS
+
+
+
+// --------------------- EXERCÍCIOS DE FUNÇÕES DE ARRAY
+
+
+
