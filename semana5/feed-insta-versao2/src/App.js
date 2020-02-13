79244df3483b34a3a97fbc8327c00b0648c18ef9
Rosana Rezende <contato@rosanarezende.com>
2020-02-13 16:36:31 -0300

    Entrega incompleta: fiz o formulários e a lista, mas ainda não aparece na tela. Vou estilizar com calma mais tarde

--- semana5/feed-insta-versao2/src/App.js
+++ semana5/feed-insta-versao2/src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styled from 'styled-components'
+
+import Header from './Components/Header/Header'
+import FormularioPost from './Components/FormularioPost/FormularioPost';
+import Post from './Components/Post/Post';
+
+const Container = styled.div`
+  background-color: #FAFAFA;
+  color: rgba(var(--i1d,38,38,38),1);
+  margin: 0;
+`
+
+const MainEstilo = styled.div`
+  margin-top: 7vh;
+  padding: 1vh 1vw;
+  min-height: 91vh;
+`
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: [{
+        imagemPerfilForm: '',
+        autorForm: '',
+        imagemPostForm: ''
+      }]
+    }
+  }  
+
+  onCriaPost = (novoPost) => {
+    console.log(novoPost)
+  }
+
+
+  render() {
+
+    const listaDePost = this.state.posts.map((cadapost) => {
+      return (<Post 
+        imagemPerfil={cadapost.imagemPerfilForm} 
+        autor={cadapost.autorForm} 
+        imagemPost={cadapost.imagemPostForm}
+      />)
+    })
+
+    return (
+
+      <Container>
+
+        <Header />
+
+        <MainEstilo>
+          <FormularioPost onCriaPost={this.onCriaPost}/>
+          {listaDePost}
+        </MainEstilo>
+
+      </Container>
+
+    )
+  }
 }
 
 export default App;
