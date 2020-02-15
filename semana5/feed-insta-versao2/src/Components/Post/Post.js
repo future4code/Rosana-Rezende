437b14bea70e53792a6dddac18ddc9375d40a9e3
Rosana Rezende <contato@rosanarezende.com>
2020-02-15 14:38:22 -0300

    Post.js estilizado com styled-components

--- semana5/feed-insta-versao2/src/Components/Post/Post.js
+++ semana5/feed-insta-versao2/src/Components/Post/Post.js
@@ -1,5 +1,73 @@
 import React from 'react'
-import './Post.css'
+import styled from 'styled-components'
+
+const PostContainer = styled.div`
+    border: 1px solid rgba(var(--b6a,219,219,219),1);
+    background-color: #FFFFFF;
+    border-radius: 2px;
+    margin: 2vh auto;
+    width: 40vw;
+`
+
+const PostHeader = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 1vh 0;
+`
+
+const Avatar = styled.img`
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    padding: 0 1vw;
+`
+
+const PostMain = styled.div``
+
+const ImagemPrincipal = styled.div`
+    display: block;
+    margin: auto;
+    width: 100%;
+`
+
+const PostFooter = styled.div``
+
+const ClicaCurtirEComentar = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 1vh 1vw;
+`
+
+const TudoCentralizado = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const ComentariosInputEBotao = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; 
+    /* justify-content: flex-end; */
+    /* align-items: flex-end; */
+`
+
+const InputEBotao = styled.div``
+
+const Input = styled.input`
+    width: 80%;
+    /* font-size: 70%; */
+`
+
+const Button = styled.button`
+    width: 18%;
+    margin-left: 1%;
+    /* font-size: 70%; */
+`
+
+const ApareceComentarios = styled.div`
+    margin: 5%;
+`
+
 
 class Post extends React.Component {
 
@@ -9,7 +77,7 @@ class Post extends React.Component {
             iconeFavorite: 'favorite_border',
             curtidas: true,
             quantidadeDeCurtidas: 0,
-            cor: {color:"black"},
+            cor: { color: "black" },
             quantidadeDeComentarios: 0,
             comentario: false,
         }
@@ -20,14 +88,14 @@ class Post extends React.Component {
             this.setState({
                 quantidadeDeCurtidas: this.state.quantidadeDeCurtidas + 1,
                 iconeFavorite: 'favorite',
-                cor: {color:"red"},
+                cor: { color: "red" },
                 curtidas: false
             })
         } else {
             this.setState({
                 quantidadeDeCurtidas: this.state.quantidadeDeCurtidas - 1,
                 iconeFavorite: 'favorite_border',
-                cor: {color:"black"},
+                cor: { color: "black" },
                 curtidas: true
             })
         }
@@ -38,14 +106,14 @@ class Post extends React.Component {
             this.setState({
                 quantidadeDeCurtidas: this.state.quantidadeDeCurtidas + 1,
                 iconeFavorite: 'favorite',
-                cor: {color:"red"},
+                cor: { color: "red" },
                 curtidas: false
             })
         } else {
             this.setState({
                 quantidadeDeCurtidas: this.state.quantidadeDeCurtidas - 1,
                 iconeFavorite: 'favorite_border',
-                cor: {color:"black"},
+                cor: { color: "black" },
                 curtidas: true
             })
         }
@@ -57,13 +125,13 @@ class Post extends React.Component {
         })
     }
 
-    apareceComentario = () => {
+    apareceInputEBotao = () => {
         if (this.state.comentario) {
             return (
-                <div className='escrever-comentario'>
-                    <input placeholder='Escreva seu comentário' onChange={ this.conteudoInput} value={ this.state.meucomentario }></input>
-                    <button onClick={this.jaComentou}>Enviar</button>
-                </div>
+                <InputEBotao>
+                    <Input placeholder='Escreva seu comentário' onChange={this.conteudoInput} value={this.state.meucomentario}/>
+                    <Button onClick={this.criaComentario}>Enviar</Button>
+                </InputEBotao>
             )
         }
     }
@@ -75,7 +143,7 @@ class Post extends React.Component {
     }
 
 
-    jaComentou = (event) => {
+    criaComentario = (event) => {
         this.setState({
             comentario: false,
             quantidadeDeComentarios: this.state.quantidadeDeComentarios + 1,
@@ -86,52 +154,52 @@ class Post extends React.Component {
     render() {
 
         return (
-            <div className='Post'>
+            <PostContainer>
 
-                <div className='cada-postagem'>
-                    <div className='cabeçalho'>
-                        <img src={ this.props.imagemPerfil } alt='' />
-                        <span>{this.props.autor}</span>
-                    </div>
-
-                    <div className='imagem-post'>
-                        <img src={ this.props.imagemPost } alt='' onDoubleClick={this.DuploClique} />
-                    </div>
-
-                    <div className='interacao'>
-
-                        <div className='curtidas-comentarios'>
-
-                            <div className='curtidas'>
-                                <i onClick={this.clicouNoCurtir} 
-                                    className="material-icons" 
-                                    style={ this.state.cor }> 
-                                    {this.state.iconeFavorite} 
-                                </i>
-                                {this.state.quantidadeDeCurtidas}
-                            </div>
-
-                            <div className='comentarios'>
-                                <i onClick={this.clicouNoComentar} className="material-icons">feedback</i>
-                                {this.state.quantidadeDeComentarios}
-                            </div>
-
-                        </div>
-
-                        <div className='aparece-comentario'>
-                            {this.apareceComentario()}
-                        </div>
-
-                        <div className='comentario-enviado'>
-                            { this.state.meucomentario }
-                        </div>
-
-                    </div>
-
-                </div>
+                <PostHeader>
+                    <Avatar src={this.props.imagemPerfil} alt='' />
+                    <span>{this.props.autor}</span>
+                </PostHeader>
 
 
-            </div>
+                <PostMain>
+                    <ImagemPrincipal src={this.props.imagemPost} alt='' onDoubleClick={this.DuploClique} />
+                </PostMain>
+
+
+                <PostFooter>
+
+                    <ClicaCurtirEComentar>
+
+                        <TudoCentralizado>
+                            <i onClick={this.clicouNoCurtir}
+                                className="material-icons"
+                                style={this.state.cor}>
+                                {this.state.iconeFavorite}
+                            </i>
+                            {this.state.quantidadeDeCurtidas}
+                        </TudoCentralizado>
+
+                        <TudoCentralizado>
+                            <i onClick={this.clicouNoComentar} className="material-icons">feedback</i>
+                            {this.state.quantidadeDeComentarios}
+                        </TudoCentralizado>
+
+                    </ClicaCurtirEComentar>
+
+                    <ComentariosInputEBotao>
+                        
+                        <ApareceComentarios>
+                            {this.state.meucomentario}
+                        </ApareceComentarios>
+                        
+                        {this.apareceInputEBotao()}
+                        
+                    </ComentariosInputEBotao>
+
+                </PostFooter>
+
+            </PostContainer>
         )
     }
 
