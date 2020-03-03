593664bc7c8da3cbbdbc0717b736dc55c5b2f2a6
Rosana Rezende <rezende_rosana@hotmail.com>
2020-03-03 20:34:45 -0300

    Corrigindo erro ao excluir o último usuário

--- semana7/cadastroDeUsuarios/app/src/Components/TelaListaDeUsuarios.js
+++ semana7/cadastroDeUsuarios/app/src/Components/TelaListaDeUsuarios.js
@@ -2,31 +2,32 @@ import React, { Component } from 'react';
 
 import styled from 'styled-components'
 import axios from 'axios'
+import DetalheDoUsuario from './DetalheDoUsuario';
 
 const TelaListaDeUsuariosContainer = styled.div`
-    padding: 1vh 1vw;    
+	padding: 1vh 1vw;    
 `
 
 const H2 = styled.h2`
-    text-align: center;
+	text-align: center;
 `
 
 const LI = styled.li`
-    padding: 1vh 0;
-    width: 30vw;
-    display: flex;
-    justify-content: space-between;
-    margin: auto;
+	padding: 1vh 0;
+	width: 30vw;
+	display: flex;
+	justify-content: space-between;
+	margin: auto;
 `
 
 const BotaoDeletar = styled.button`
-    font-family: 'Roboto', sans-serif;
-    font-size: 1rem;
-    border-radius: 50px;
-    outline: 0;
-    padding: 5px 10px;
-    background-color: rgba(0, 0, 0, 0.9);
-    color: white;
+	font-family: 'Roboto', sans-serif;
+	font-size: 1rem;
+	border-radius: 50px;
+	outline: 0;
+	padding: 5px 10px;
+	background-color: rgba(0, 0, 0, 0.9);
+	color: white;
 `
 
 const baseUrl = "https://us-central1-future4-users.cloudfunctions.net/api";
@@ -35,89 +36,104 @@ const authToken = "rosanarezende"; // Só para evitar repetição.
 
 class TelaListaDeUsuarios extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            todosOsUsuarios: [],
-        }
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			todosOsUsuarios: [],
+			// mostraLista: true,
+			mostraDetalhes: false,
+		}
+	}
 
-    componentDidMount() {
-        this.buscarTodosOsUsuarios()
-    }
+	componentDidMount() {
+		this.buscarTodosOsUsuarios()
+	}
 
-    buscarTodosOsUsuarios = () => {
+	buscarTodosOsUsuarios = () => {
 
-        const todosOsUsuariosPromessa = axios.get(
-            `${baseUrl}/users/getAllUsers`,
-            {
-                headers: {
-                    'api-token': authToken
-                }
-            }
-        )
+		const todosOsUsuariosPromessa = axios.get(
+			`${baseUrl}/users/getAllUsers`,
+			{
+				headers: {
+					'api-token': authToken
+				}
+			}
+		)
 
-        todosOsUsuariosPromessa
-            .then(response => {
-                this.setState({ todosOsUsuarios: response.data.result })
-            })
-            .catch(error => {
-                console.log(error)
-            })
+		todosOsUsuariosPromessa
+			.then(response => {
+				this.setState({ todosOsUsuarios: response.data.result })
+			})
+			.catch(error => {
+				console.log(error.response.data.message)
+				this.setState({ todosOsUsuarios: [] })
+			})
 
-    }
+	}
 
-    deletaUsuario = (idDoUsuario) => {
-            
-        const deletar = window.confirm('Tem certeza de que deseja deletar?')
+	deletaUsuario = (idDoUsuario) => {
 
-        if (deletar) {
+		const deletar = window.confirm('Tem certeza de que deseja deletar?')
 
-            const usuarioDeletaPromessa = axios.delete(
-                `${baseUrl}/users/deleteUser?id=${idDoUsuario}`,
-                {
-                    headers: {
-                        'api-token': authToken
-                    }
-                }
-                )
+		if (deletar) {
 
-            usuarioDeletaPromessa
-            .then(response => {
-                alert('Usuário deletado com sucesso')
-                this.buscarTodosOsUsuarios();
-            })
-            .catch(error => {
-                alert(error)
-            })
-        }
+			const usuarioDeletaPromessa = axios.delete(
+				`${baseUrl}/users/deleteUser?id=${idDoUsuario}`,
+				{
+					headers: {
+						'api-token': authToken
+					}
+				}
+			)
 
-        
-    }
+			usuarioDeletaPromessa
+				.then(response => {
+					alert('Usuário deletado com sucesso')
+					this.buscarTodosOsUsuarios();
+				})
+				.catch(error => {
+					alert(error)
+				})
+		}
 
-    render() {
-        return (
-            <TelaListaDeUsuariosContainer>
-                <H2>Lista de Usuários</H2>
+	}
 
-                {this.state.todosOsUsuarios.length === 0 && <p>Carregando...</p>}
+	mostraDetalheDoUsuario = () => {
+		this.setState({ mostraDetalhes: !this.state.mostraDetalhes })
+	}
 
-                <ul>
-                    {this.state.todosOsUsuarios.map(usuario => (
-                        <LI key={usuario.id}>
-                            <span>{usuario.name}   </span>
-                            <BotaoDeletar
-                                onClick={() => this.deletaUsuario(usuario.id)}
-                            >
-                                X
-                            </BotaoDeletar>
-                        </LI>
-                    ))}
-                </ul>
+	render() {
 
-            </TelaListaDeUsuariosContainer>
-        );
-    }
+		const listaDeUsuarios = (
+			<ul>
+				{this.state.todosOsUsuarios.map(usuario => (
+					<LI key={usuario.id}>
+						<span onClick={() => this.mostraDetalheDoUsuario(usuario.id)}>{usuario.name}   </span>
+						<BotaoDeletar
+							onClick={() => this.deletaUsuario(usuario.id)}
+						>
+							X
+							</BotaoDeletar>
+					</LI>
+					
+				))}
+			</ul>
+		)
+
+		return (
+			<TelaListaDeUsuariosContainer>
+				<H2>Lista de Usuários</H2>
+
+				{this.state.todosOsUsuarios.length === 0 && <p>Carregando...</p>}
+
+				{/* {this.state.mostraLista ? listaDeUsuarios : ""} */}
+				{listaDeUsuarios}
+
+				{this.state.mostraDetalhes  && <DetalheDoUsuario/>}
+
+			</TelaListaDeUsuariosContainer>
+		);
+	}
 }
 
 export default TelaListaDeUsuarios;
