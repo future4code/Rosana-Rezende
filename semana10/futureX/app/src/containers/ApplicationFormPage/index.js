d3c660bde7682d6df47474ca0f6525df16afd877
Rosana Rezende <rezende_rosana@hotmail.com>
2020-03-24 12:44:55 -0300

    Iniciando refatoração

--- semana10/futureX/app/src/containers/ApplicationFormPage/index.js
+++ semana10/futureX/app/src/containers/ApplicationFormPage/index.js
@@ -47,12 +47,7 @@ class ApplicationFormPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: undefined,
-      age: undefined,
-      text: undefined,
-      profession: undefined,
-      country: undefined,
-      tripSelectedId: undefined
+      form: {}
     };
   }
 
@@ -62,53 +57,87 @@ class ApplicationFormPage extends Component {
 
   handleFieldChange = event => {
     this.setState({
-      [event.target.name]: event.target.value
+      form: {
+        ...this.state.form,
+        [event.target.name]: event.target.value
+      }
     });
   };
 
-  toAplly = (e) => {
-    e.preventDefault()
-    const newName = this.state.name
-    const newAge = this.state.age
-    const newText = this.state.text
-    const newProfession = this.state.profession
-    const newCountry = this.state.country
-    const id = this.state.tripSelectedId
-
-    if (newName && newAge && newText && newProfession && newCountry && id) {
-      const application = {
-        name: newName,
-        age: newAge,
-        applicationText: newText,
-        profession: newProfession,
-        country: newCountry
-      }
-
-      console.log(application)
-      console.log(id) 
-      // this.props.applyToTrip(id, application)
-
-      this.setState({
-        name: '',
-        age: '',
-        text: '',
-        profession: '',
-        country: '',
-        tripSelectedId: ''
-      })
-
-    } else {
-      alert('Preencha todos os campos para aplicar para uma viagem')
-    }
-
-  }
+  handleSubmission = event => {
+    event.preventDefault();
+    console.log(this.state.form);
+    // this.props.minhaAcao(this.state.form)
+    // this.props.applyToTrip(id, application)
+  };
 
   render() {
 
     const { classes, goToHome, goToLogin, trips } = this.props
     // console.log(trips)
 
-    const { name, age, text, profession, country, tripSelectedId } = this.state
+    const formFields = [
+      { 
+        type: 'text', 
+        label: 'Nome', 
+        name: 'name', 
+        title: '', 
+        required: true, 
+        pattern: '', 
+      },
+      { 
+        type: 'number', 
+        label: 'Idade', 
+        name: 'age', 
+        title: '', 
+        required: true, 
+        pattern: '', 
+        min: 18
+      },
+      { 
+        type: 'text', 
+        label: 'Texto de aplicação', 
+        name: 'text', 
+        title: '', 
+        required: true, 
+        pattern: '', 
+        multiline: true,
+        rows: '4'
+      },
+      { 
+        type: 'text', 
+        label: 'Profissão', 
+        name: 'profession', 
+        title: '', 
+        required: true, 
+        pattern: '', 
+      },
+    ]
+
+    const coutrys = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+
+    const formSelects = [
+      {
+        type: 'text', 
+        label: 'País', 
+        name: 'country', 
+        title: '', 
+        required: true, 
+        pattern: '', 
+        helperText: 'Selecione um país' ,
+        options: coutrys
+      },
+      {
+        type: 'text', 
+        label: 'Viagem', 
+        name: 'tripSelectedId', 
+        title: '', 
+        required: true, 
+        pattern: '', 
+        helperText: 'Selecione uma viagem' ,   
+        options: trips
+      },
+    ]
 
     return (
       <>
@@ -122,120 +151,78 @@ class ApplicationFormPage extends Component {
             </Toolbar>
           </AppBar>
       
-      <ApplicationFormWrapper>
+      <ApplicationFormWrapper onSubmit={this.handleSubmission}>
         
         <Typography variant="h6" color="inherit">
-        Preencha os dados abaixo para se candidata a uma viagem:
+        Preencha os dados abaixo para se candidatar a uma viagem:
         </Typography>
-        
-        <TextField
-          // id="outlined-name"
-          label="Nome"
-          margin="normal"
-          variant="outlined"
-          type='text'
-          name='name'
-          value={name}
-          onChange={this.handleFieldChange}
-          fullWidth
-        />
 
-        <TextField
-          // id="outlined-name"
-          label="Idade"
-          margin="normal"
-          variant="outlined"
-          type='number'
-          name='age'
-          value={age}
-          onChange={this.handleFieldChange}
-          fullWidth
-        />
+        {formFields.map(field => (
+          <TextField
+            key={field.name}
+            id={field.name}
+            name={field.name}
+            type={field.type}
+            label={field.label}
+            title={field.title}
+            required={field.required}
+            pattern={field.pattern}
+            multiline={field.multiline}
+            rows={field.rows}
+            min={field.min}
+            margin='normal'
+            variant='outlined'
+            fullWidth
+            value={this.state.form[field.name]}
+            onChange={this.handleFieldChange}
+          />
+        ))}
 
-        <TextField
-          // id="outlined-multiline-static"
-          label="Texto de aplicação"
-          multiline
-          rows="4"
-          margin="normal"
-          variant="outlined"
-          type='text'
-          name='text'
-          value={text}
-          onChange={this.handleFieldChange}
-          fullWidth
-        />
-
-        <TextField
-          // id="outlined-name"
-          label="Profissão"
-          margin="normal"
-          variant="outlined"
-          type='text'
-          name='profession'
-          value={profession}
-          onChange={this.handleFieldChange}
-          fullWidth
-        />
-
-        <TextField
-          // id="outlined-name"
-          label="País"
-          margin="normal"
-          variant="outlined"
-          type='text'
-          name='country'
-          value={country}
-          onChange={this.handleFieldChange}
-          fullWidth
-        />
-
-        <TextField
-          // id="outlined-name"
-          label="Viagem"
-          margin="normal"
-          variant="outlined"
-          // type='text'
-          name='tripSelectedId'
-          value={tripSelectedId}
-          onChange={this.handleFieldChange}
-          fullWidth
-          select
-          SelectProps={{
-            MenuProps: {
-              className: classes.menu,
-            },
-          }}
-          helperText="Selecione uma viagem"
-        > 
-          <option value='' hidden></option>
-          {trips.map(trip => {
-            return (
-              <MenuItem key={trip.id} value={trip.id}>{trip.name}</MenuItem >
-            )
-          })}
-        </TextField>
-
-
-        {/* Viagem:
-        <select
-          name='tripSelectedId'
-          value={tripSelectedId}
-          onChange={this.handleFieldChange}
-        >
-          <option value='' hidden>Selecione...</option>
-          {trips.map(trip => {
-            return (
-              <option value={trip.id}>{trip.name}</option>
-            )
-          })}
-        </select> */}
+        {formSelects.map(select => (
+          <TextField
+            key={select.name}
+            id={select.name}
+            name={select.name}
+            type={select.type}
+            label={select.label}
+            title={select.title}
+            required={select.required}
+            pattern={select.pattern}
+            margin='normal'
+            variant='outlined'
+            fullWidth
+            value={this.state.form[select.name]}
+            onChange={this.handleFieldChange}        
+            select  
+            SelectProps={{
+              MenuProps: {
+                className: classes.menu,
+              },
+            }}
+            helperText={select.helperText}          
+          >
+            {/* <MenuItem value='' hidden></MenuItem> */}
+            {select.options === trips ? 
+              select.options.map(option => {
+              return (
+                <MenuItem key={option.id} value={option.id}>{option.name} - {option.planet}</MenuItem >
+              )
+            })
+            : 
+            select.options.map(option => {
+              return (
+                <MenuItem key={option} value={option}>{option}</MenuItem >
+              )
+            })
+            }
+          </TextField>
+        ))}
 
         <Button
           variant='contained' 
           color='primary' 
           className={classes.button}
-          onClick={this.toAplly}
+          type='submit'
         >
           Aplicar
         </Button>
