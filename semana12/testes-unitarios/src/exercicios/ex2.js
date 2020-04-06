4628c6eeabd2ba16ba6791cca51adf62bd65d48a
Rosana Rezende <rezende_rosana@hotmail.com>
2020-04-06 14:03:34 -0300

    Exercício 2

--- semana12/testes-unitarios/src/exercicios/ex2.js
+++ semana12/testes-unitarios/src/exercicios/ex2.js
@@ -1,3 +1,10 @@
 export function checaPalindromo(frase) {
-    
+    let palindromo = false
+    let fraseSemTraco = frase.replace(/-/g, " ")
+    let fraseSemEspacos = fraseSemTraco.replace(/ /g, "");
+    let novaFrase = fraseSemEspacos.split('').reverse().join('')   
+    if(fraseSemEspacos.toLowerCase() === novaFrase.toLowerCase()){
+        palindromo = true
+    }
+    return palindromo
 }
--- semana12/testes-unitarios/src/exercicios/ex2.test.js
+++ semana12/testes-unitarios/src/exercicios/ex2.test.js
@@ -1,7 +1,22 @@
 import { checaPalindromo } from "./ex2";
 
 describe("Checa Palíndromo", () => {
-  it("Primeiro teste", () => {
-    checaPalindromo();
+  it("Palíndromo - Retorna true quando nome = mirim", () => {
+    const nome = "mirim"
+    const resultado = checaPalindromo(nome);
+    expect(resultado).toBe(true)
   });
+
+  it("Palíndromo - Retorna true quando frase = Socorram-me subi no onibus em marrocos", () => {
+    const frase = "Socorram-me subi no onibus em marrocos"
+    const resultado = checaPalindromo(frase);
+    expect(resultado).toBe(true)
+  });
+
+  it("Não é palíndromo - Retorna false quando frase = nada", () => {
+    const frase = "nada"
+    const resultado = checaPalindromo(frase);
+    expect(resultado).toBe(false)
+  });
+
 });
