cf08710a29b710a64f0292c44be0a2eaa68c6f9a
Rosana Rezende <rezende_rosana@hotmail.com>
2020-04-09 11:16:01 -0300

    useEffect chama o getTasks - testado

--- semana12/planner/app/src/components/Tasks/index.test.js
+++ semana12/planner/app/src/components/Tasks/index.test.js
@@ -1,45 +1,26 @@
+// https://github.com/enzymejs/enzyme/issues/2086
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import { Tasks } from './index'
-import { } from './styles'
-
-import configureStore from 'redux-mock-store'
-
-const mockStore = configureStore([])
-const initialState = {};
-
-const mockTasks = []
 
 describe('List Tasks - when obtained task list', () => {
-    let store
     let component
-    let useEffect;
-
-    const mockGetTasks = jest.fn()     
-
-    const mockUseEffect = () => {
-        useEffect.mockImplementationOnce(f => f());
-    };
+    const mockTasks = []
+    const mockGetTasks = jest.fn()
 
     beforeEach(() => {
-        store = mockStore(initialState);
-        component = shallow(<Tasks store={store}
-        getTasks={mockGetTasks} 
-        tasks={mockTasks}
-        />)
+
+        jest.spyOn(React, "useEffect").mockImplementation(f => f());
+
+        component = mount(
+            <Tasks
+                tasks={mockTasks}
+                getTasks={mockGetTasks}
+            />)
     });
 
-    /* mocking useEffect */
-    useEffect = jest.spyOn(React, "useEffect");
-    mockUseEffect(); // 2 times
-    mockUseEffect(); //
-
     it('should call de mock get Task function', () => {
-        const actions = store.getActions();
-
-        // não sei prosseguir
-
-        // expect(actions).toEqual([{ type: "GET_TASKS" }]);
+        expect(mockGetTasks).toHaveBeenCalled()
     });
 
 })
