a3cad26de1551084b4e9e99dfa7d2c634ed20078
Rosana Rezende <rezende_rosana@hotmail.com>
2020-04-23 19:30:15 -0300

    desafio 2

--- semana14/aprofundamento-Promises/build/exercicio2-1.js
+++ semana14/aprofundamento-Promises/build/exercicio2-1.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = require("axios");
-const baseUrl = 'https://jsonplaceholder.typicode.com/posts';
+const baseUrl = 'https://jsonplaceholder.typicode.com';
 const operation = process.argv[4];
 const postNumber = process.argv[5];
 const postNumber2 = process.argv[6];
 const postNumber3 = process.argv[7];
 if (operation === 'getPost') {
-    const post = axios_1.default.get(`${baseUrl}/${postNumber}`);
+    const post = axios_1.default.get(`${baseUrl}/posts/${postNumber}`);
     post.then(result => {
         console.log(result.data);
     })
@@ -16,14 +16,23 @@ if (operation === 'getPost') {
     });
 }
 else if (operation === 'getPosts') {
-    const post1 = axios_1.default.get(`${baseUrl}/${postNumber}`);
-    const post2 = axios_1.default.get(`${baseUrl}/${postNumber2}`);
-    const post3 = axios_1.default.get(`${baseUrl}/${postNumber3}`);
+    const post1 = axios_1.default.get(`${baseUrl}/posts/${postNumber}`);
+    const post2 = axios_1.default.get(`${baseUrl}/posts/${postNumber2}`);
+    const post3 = axios_1.default.get(`${baseUrl}/posts/${postNumber3}`);
     Promise.all([post1, post2, post3])
         .then((result) => {
         result.map(post => {
             console.log(post.data);
         });
+    })
+        .catch(err => {
+        console.error(err);
+    });
+}
+else if (operation === 'getPostComments') {
+    const postComments = axios_1.default.get(`${baseUrl}/comments?postId=${postNumber}`);
+    postComments.then(result => {
+        console.log(result.data);
     })
         .catch(err => {
         console.error(err);
--- semana14/aprofundamento-Promises/src/exercicio2-1.ts
+++ semana14/aprofundamento-Promises/src/exercicio2-1.ts
@@ -1,13 +1,13 @@
 import axios, {AxiosResponse} from 'axios'
 
-const baseUrl: string = 'https://jsonplaceholder.typicode.com/posts'
+const baseUrl: string = 'https://jsonplaceholder.typicode.com'
 const operation: string = process.argv[4]
 const postNumber: string = process.argv[5]
 const postNumber2: string = process.argv[6]
 const postNumber3: string = process.argv[7]
 
 if(operation === 'getPost'){
-    const post: Promise<AxiosResponse> = axios.get(`${baseUrl}/${postNumber}`)
+    const post: Promise<AxiosResponse> = axios.get(`${baseUrl}/posts/${postNumber}`)
     
     post.then( result => {
             console.log(result.data)
@@ -17,15 +17,25 @@ if(operation === 'getPost'){
         })
 } 
 else if(operation === 'getPosts'){
-    const post1: Promise<AxiosResponse> = axios.get(`${baseUrl}/${postNumber}`)
-    const post2: Promise<AxiosResponse> = axios.get(`${baseUrl}/${postNumber2}`)
-    const post3: Promise<AxiosResponse> = axios.get(`${baseUrl}/${postNumber3}`)
+    const post1: Promise<AxiosResponse> = axios.get(`${baseUrl}/posts/${postNumber}`)
+    const post2: Promise<AxiosResponse> = axios.get(`${baseUrl}/posts/${postNumber2}`)
+    const post3: Promise<AxiosResponse> = axios.get(`${baseUrl}/posts/${postNumber3}`)
 
     Promise.all([post1, post2, post3])
         .then( (result: AxiosResponse[]) => {
             result.map(post => {
                 console.log(post.data)
             })
+        })
+        .catch(err => {
+            console.error(err)
+        })
+}
+else if(operation === 'getPostComments'){
+    const postComments: Promise<AxiosResponse> = axios.get(`${baseUrl}/comments?postId=${postNumber}`)
+    
+    postComments.then( result => {
+            console.log(result.data)
         })
         .catch(err => {
             console.error(err)
