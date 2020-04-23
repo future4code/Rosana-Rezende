2830515c14b40d9b2df0081f7c2f9baa7904bdd2
Rosana Rezende <rezende_rosana@hotmail.com>
2020-04-23 19:23:58 -0300

    desafio 1

--- semana14/aprofundamento-Promises/build/exercicio2-1.js
+++ semana14/aprofundamento-Promises/build/exercicio2-1.js
@@ -4,6 +4,8 @@ const axios_1 = require("axios");
 const baseUrl = 'https://jsonplaceholder.typicode.com/posts';
 const operation = process.argv[4];
 const postNumber = process.argv[5];
+const postNumber2 = process.argv[6];
+const postNumber3 = process.argv[7];
 if (operation === 'getPost') {
     const post = axios_1.default.get(`${baseUrl}/${postNumber}`);
     post.then(result => {
@@ -13,7 +15,21 @@ if (operation === 'getPost') {
         console.error(err);
     });
 }
+else if (operation === 'getPosts') {
+    const post1 = axios_1.default.get(`${baseUrl}/${postNumber}`);
+    const post2 = axios_1.default.get(`${baseUrl}/${postNumber2}`);
+    const post3 = axios_1.default.get(`${baseUrl}/${postNumber3}`);
+    Promise.all([post1, post2, post3])
+        .then((result) => {
+        result.map(post => {
+            console.log(post.data);
+        });
+    })
+        .catch(err => {
+        console.error(err);
+    });
+}
 else {
-    console.error('\x1b[31m', 'Faça uma operação válida');
+    console.log('\x1b[31m', 'Faça uma operação válida');
 }
 //# sourceMappingURL=exercicio2-1.js.map
--- semana14/aprofundamento-Promises/src/exercicio2-1.ts
+++ semana14/aprofundamento-Promises/src/exercicio2-1.ts
@@ -3,6 +3,8 @@ import axios, {AxiosResponse} from 'axios'
 const baseUrl: string = 'https://jsonplaceholder.typicode.com/posts'
 const operation: string = process.argv[4]
 const postNumber: string = process.argv[5]
+const postNumber2: string = process.argv[6]
+const postNumber3: string = process.argv[7]
 
 if(operation === 'getPost'){
     const post: Promise<AxiosResponse> = axios.get(`${baseUrl}/${postNumber}`)
@@ -13,6 +15,22 @@ if(operation === 'getPost'){
         .catch(err => {
             console.error(err)
         })
-} else {
+} 
+else if(operation === 'getPosts'){
+    const post1: Promise<AxiosResponse> = axios.get(`${baseUrl}/${postNumber}`)
+    const post2: Promise<AxiosResponse> = axios.get(`${baseUrl}/${postNumber2}`)
+    const post3: Promise<AxiosResponse> = axios.get(`${baseUrl}/${postNumber3}`)
+
+    Promise.all([post1, post2, post3])
+        .then( (result: AxiosResponse[]) => {
+            result.map(post => {
+                console.log(post.data)
+            })
+        })
+        .catch(err => {
+            console.error(err)
+        })
+}
+else {
     console.log('\x1b[31m' , 'Faça uma operação válida')
 }
