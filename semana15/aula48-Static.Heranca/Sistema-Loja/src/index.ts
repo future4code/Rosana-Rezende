c4ab4aa02c03d0bad3cde6ca1259a774dd0bad68
Rosana Rezende <rezende_rosana@hotmail.com>
2020-04-28 14:49:43 -0300

    Exercício 6

--- semana15/aula48-Static.Heranca/Sistema-Loja/src/index.ts
+++ semana15/aula48-Static.Heranca/Sistema-Loja/src/index.ts
@@ -4,34 +4,34 @@ import { Employee } from './Employee'
 
 // RESPOSTAS DOS EXERCÍCIOS DA TARDE
 
-// ------------------------------ EXERCÍCIO 1 ------------------------------
+// // ------------------------------ EXERCÍCIO 1 ------------------------------
 
 const newUser = new User("oioioi", "email@email.com", "Oiee", "ioioio")
 // console.log("Id:", newUser.getId())
 // console.log("Nome:", newUser.getName())
 // console.log("Email:", newUser.getEmail())
 
-// a. *Seria possível imprimir a senha (`password`) atrelada a essa instância?*
+// // a. *Seria possível imprimir a senha (`password`) atrelada a essa instância?*
 // Não, pois trata-se de um atributo privado, que não tem um getter relacionado
 
-// b. Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal?
+// // b. Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal?
 // Apenas 1 vez, antes dos 3 consoles acima
 
 
 
-// ------------------------------ EXERCÍCIO 2 ------------------------------
+// // ------------------------------ EXERCÍCIO 2 ------------------------------
 
 const newConsumer = new Consumer("oieoie", "oie@email.com", "Oie", "eioeio", "010101")
 
-// a. Quantas vezes a mensagem "Chamando o construtor da classe Customer" foi impressa no terminal? 
+// // a. Quantas vezes a mensagem "Chamando o construtor da classe Customer" foi impressa no terminal? 
 // Uma vez
 
-// b. Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal? Por quê?
+// // b. Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal? Por quê?
 // Duas vezes. A primeira quando eu instanciei o newUser e a segunda quando instancie o newConsumer. Acredito que esse segundo chamado aconteceça justamente por Consumer ser um filho de User, logo quando o filho é chamado o pai também é
 
 
 
-// ------------------------------ EXERCÍCIO 3 ------------------------------
+// // ------------------------------ EXERCÍCIO 3 ------------------------------
 
 // console.log(`
 // Id: ${newConsumer.getId()}
@@ -41,21 +41,25 @@ const newConsumer = new Consumer("oieoie", "oie@email.com", "Oie", "eioeio", "01
 // Cartão de crédito: ${newConsumer.getCreditCard()}
 // `)
 
-// a. Seria possível imprimir a senha (password) atrelada a essa instância? Por quê?
+// // a. Seria possível imprimir a senha (password) atrelada a essa instância? Por quê?
 // Não é possível, pois ele não consegue acessar propriedades privadas da classe pai
 
 
 
-// ------------------------------ EXERCÍCIO 4 e 5 ------------------------------
+// // ------------------------------ EXERCÍCIO 4 e 5 ------------------------------
 
 // newConsumer.introduceYourself()
 
 
 
-// ------------------------------ EXERCÍCIO 6 ------------------------------
+// // ------------------------------ EXERCÍCIO 6 ------------------------------
 
 const newEmployee = new Employee("olaola", "ola@email.com", "Olá", "aloalo", 1200, "10/01/2019")
 
+// // a. Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal?
+// Três vezes
+
+// // b. Imprima as informações dessa instância no terminal. Quais dados são possíveis de serem impressos?
 // console.log(`
 // Id: ${newEmployee.getId()}
 // Nome: ${newEmployee.getName()}
@@ -65,21 +69,23 @@ const newEmployee = new Employee("olaola", "ola@email.com", "Olá", "aloalo", 12
 // `)
 // newEmployee.introduceYourself()
 
-// ------------------------------ EXERCÍCIO 7 ------------------------------
+
+
+// // ------------------------------ EXERCÍCIO 7 ------------------------------
 
 
 
-// ------------------------------ EXERCÍCIO 8 ------------------------------
+// // ------------------------------ EXERCÍCIO 8 ------------------------------
 
 
 
-// ------------------------------ EXERCÍCIO 9 ------------------------------
+// // ------------------------------ EXERCÍCIO 9 ------------------------------
 
 
-// ------------------------------ EXERCÍCIO 10 ------------------------------
+// // ------------------------------ EXERCÍCIO 10 ------------------------------
 
 
 
-// ------------------------------ EXERCÍCIO 11 ------------------------------
+// // ------------------------------ EXERCÍCIO 11 ------------------------------
 
 
