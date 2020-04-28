63b70dd79fcf7948a215fec2a70de75fa64c7adf
Rosana Rezende <rezende_rosana@hotmail.com>
2020-04-28 13:45:54 -0300

    Exercício 2

--- semana15/aula48-Static.Heranca/Sistema-Loja/src/Consumer.ts
+++ semana15/aula48-Static.Heranca/Sistema-Loja/src/Consumer.ts
@@ -1,15 +1,17 @@
 import { User } from './User'
 
 export class Consumer extends User {
-    cartNumber: number;
-    totalPurchases: number
+    public purchaseTotal: number = 0; // não precisa ir pro construtor, já demos um valor aqui
+    private creditCard: string;
 
-    constructor(id: string, email: string, name: string, password: string, cartNumber: number, totalPurchases: number){
+    constructor(id: string, email: string, name: string, password: string, creditCard: string){
         super(id, email, name, password)
-        this.cartNumber = cartNumber
-        this.totalPurchases = totalPurchases
+        console.log("Chamando o construtor da classe Customer")
+        this.creditCard = creditCard
     }
 
-
+    public getCreditCard(): string {
+        return this.creditCard
+    }
 
 }
--- semana15/aula48-Static.Heranca/Sistema-Loja/src/index.ts
+++ semana15/aula48-Static.Heranca/Sistema-Loja/src/index.ts
@@ -1,13 +1,14 @@
 import { User } from './User'
+import { Consumer } from './Consumer'
 
 // RESPOSTAS DOS EXERCÍCIOS DA TARDE
 
 // ------------------------------ EXERCÍCIO 1 ------------------------------
 
 const newUser = new User("oioioi", "email@email.com", "Oiee", "ioioio")
-console.log("Id:", newUser.getId())
-console.log("Nome:", newUser.getName())
-console.log("Email:", newUser.getEmail())
+// console.log("Id:", newUser.getId())
+// console.log("Nome:", newUser.getName())
+// console.log("Email:", newUser.getEmail())
 
 // a. *Seria possível imprimir a senha (`password`) atrelada a essa instância?*
 // Não, pois trata-se de um atributo privado, que não tem um getter relacionado
@@ -15,7 +16,18 @@ console.log("Email:", newUser.getEmail())
 // b. Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal?
 // Apenas 1 vez, antes dos 3 consoles acima
 
+
+
 // ------------------------------ EXERCÍCIO 2 ------------------------------
+
+const newConsumer = new Consumer("oieoie", "oie@email.com", "Oie", "eioeio", "010101")
+
+// a. Quantas vezes a mensagem "Chamando o construtor da classe Customer" foi impressa no terminal? 
+// Uma vez
+
+// b. Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal? Por quê?
+// Duas vezes. A primeira quando eu instanciei o newUser e a segunda quando instancie o newConsumer. Acredito que esse segundo chamado aconteceça justamente por Consumer ser um filho de User, logo quando o filho é chamado o pai também é
+
 
 
 // ------------------------------ EXERCÍCIO 3 ------------------------------
