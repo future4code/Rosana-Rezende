e39aa98c33be5d32dcec133979cc1363ed0b862e
Rosana Rezende <rezende_rosana@hotmail.com>
2020-04-29 12:39:14 -0300

    exercício 1

--- semana15/aula49-Polimorfismo/Sistema-Energia/build/index.js
+++ semana15/aula49-Polimorfismo/Sistema-Energia/build/index.js
@@ -4,9 +4,19 @@ const Residence_1 = require("./classes/Residence");
 const Commerce_1 = require("./classes/Commerce");
 const Industry_1 = require("./classes/Industry");
 const newResidence = new Residence_1.Residence(10, "29902060");
-console.log(newResidence);
 const newCommerce = new Commerce_1.Commerce(2, "29902070");
-console.log(newCommerce);
 const newIndustry = new Industry_1.Industry(5, "29902080");
-console.log(newIndustry);
+const clientFulano = {
+    name: "Fulano",
+    registrationNumber: 1,
+    consumedEnergy: 100,
+    calculateBill() {
+        return 2;
+    }
+};
+console.log(`Nome: ${clientFulano.name}
+Número de cadastro: ${clientFulano.registrationNumber}
+Energia consumida: ${clientFulano.consumedEnergy}
+Valor da conta: ${clientFulano.calculateBill()}
+`);
 //# sourceMappingURL=index.js.map
--- semana15/aula49-Polimorfismo/Sistema-Energia/src/index.ts
+++ semana15/aula49-Polimorfismo/Sistema-Energia/src/index.ts
@@ -1,6 +1,7 @@
 import { Residence } from './classes/Residence'
 import { Commerce } from './classes/Commerce'
 import { Industry } from './classes/Industry'
+import { Client } from './interfaces/Client'
 
 const newResidence = new Residence(10, "29902060")
 // console.log(newResidence)
@@ -10,3 +11,20 @@ const newCommerce = new Commerce(2, "29902070")
 
 const newIndustry = new Industry(5, "29902080")
 // console.log(newIndustry)
+
+
+// ---------------------------------- EXERCÍCIO 1 ----------------------------------
+
+const clientFulano: Client = {
+    name: "Fulano",
+    registrationNumber: 1,
+    consumedEnergy: 100,
+    calculateBill(): number {
+        return 2
+    }
+}
+console.log(`Nome: ${clientFulano.name}
+Número de cadastro: ${clientFulano.registrationNumber}
+Energia consumida: ${clientFulano.consumedEnergy}
+Valor da conta: ${clientFulano.calculateBill()}
+`)
