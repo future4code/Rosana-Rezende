1b8d02e4e339df435c4d353febbfd5648860a714
Rosana Rezende <rezende_rosana@hotmail.com>
2020-04-30 11:56:43 -0300

    criados 2 turmas, 2 alunos e 4 professores

--- semana15/projeto-LabenuSystem/LabenuSystem/src/index.ts
+++ semana15/projeto-LabenuSystem/LabenuSystem/src/index.ts
@@ -1,12 +1,34 @@
 import { NightMission } from "./classes/NightMission";
-import { Teacher } from "./classes/Teacher";
 import { FullTimetMission } from "./classes/FullTimeMission";
+import { Teacher } from "./classes/Teacher";
+import { Student } from "./classes/Student";
 
-const newNightMission = new NightMission(1, "lala-na-night", "30/04/2020", "12/12/2020", [], [], 1)
-// console.log(newNightMission)
+// CRIAR 2 TURMAS
+const turmaSaganNoturna = new NightMission(1, "Sagan-na-night", "30/04/2020", "12/12/2020", [], [], 1)
+// console.log(turmaSaganNoturna)
 
-const newFullTimeMission = new FullTimetMission(2, "lolo", "01/01/2020", "06/06/2020", [], [], 7)
+const turmaSagan = new FullTimetMission(2, "Sagan", "01/01/2020", "06/06/2020", [], [], 5)
+// console.log(turmaSagan)
 
-const newTeacher = new Teacher(1, "Prof Bananinha", "lalala", ["React"])
-// console.log(newTeacher)
 
+// CRIAR  2 ALUNOS
+const alunaRosana = new Student(1, "Rosana", "rosana@email.com", "11/03/1987", ["Maranotar Netflix", "Codar"])
+// console.log(alunaRosana)
+
+const alunoJonas = new Student(2, "Jonas", "jonas@email.com", "20/01/1995", ["Jogar bola"])
+// console.log(alunoJonas)
+
+
+// CRIAR 4 PROFESSORES
+
+const profBananinha = new Teacher(3, "Prof Bananinha", "bananinha@email.com", ["React"])
+// console.log(profBananinha)
+
+const profLaranjinha = new Teacher(4, "Prof Laranjinha", "laranjinha@email.com", ["CSS", "Testes"])
+// console.log(profLaranjinha)
+
+const profAmorzinho = new Teacher(5, "Prof Amorzinho", "amorzinho@email.com", ['React', 'Redux'])
+// console.log(profAmorzinho)
+
+const profZangado = new Teacher(6, "Prof Zangado", "zangado@email.com", ['Typescript', 'Programação Orientada a Objetos', 'Backend'])
+// console.log(profZangado)
