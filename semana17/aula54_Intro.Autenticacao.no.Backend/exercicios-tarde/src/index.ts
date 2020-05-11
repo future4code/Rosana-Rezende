ed52937a6407cf4caf48942f93f1911434261bbb
Rosana Rezende <rezende_rosana@hotmail.com>
2020-05-11 13:22:09 -0300

    estrutura

---
+++ semana17/aula54_Intro.Autenticacao.no.Backend/exercicios-tarde/src/index.ts
@@ -0,0 +1,58 @@
+import knex from "knex";
+import express, { Request, Response } from "express";
+import { AddressInfo } from "net";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connection = knex({
+    client: "mysql",
+    connection: {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT || "3306"),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    },
+});
+
+const app = express();
+app.use(express.json());
+
+// ====================================================================
+
+//conferir conteúdo de tabelas já criadas
+const getTableContent = async (table_name: string): Promise<any> => {
+    const result = connection(`${table_name}`)
+        .select("*")
+    return result
+}
+// (async () => {
+//     console.log(await getTableContent("ToDoListUser"))
+// })()
+
+// (async () => {
+//     console.log(await getTableContent("ToDoListTask"))
+// })()
+
+// (async () => {
+//     console.log(await getTableContent("ToDoListUserTaskRelation"))
+// })()
+
+
+
+
+
+
+
+
+// ====================================================================
+
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+        console.error(`Failure upon starting server.`);
+    }
+});
