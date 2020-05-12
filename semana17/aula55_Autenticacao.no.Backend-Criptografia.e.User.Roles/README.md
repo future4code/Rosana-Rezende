967742e7df1d93aa3b37872441060d8d1d45d3c0
Rosana Rezende <rezende_rosana@hotmail.com>
2020-05-12 16:20:09 -0300

    exercício 6

--- semana17/aula55_Autenticacao.no.Backend-Criptografia.e.User.Roles/README.md
+++ semana17/aula55_Autenticacao.no.Backend-Criptografia.e.User.Roles/README.md
@@ -452,7 +452,29 @@ Implemente o endpoint que retorne as informações (id e email) de um usuário a
 
 _Resposta_:
 ```ts
+app.get("/user/:id", async(req: Request, res: Response) => {
+    try{
+        const token = req.headers.authorization as string
 
+        const authenticator = new Authenticator()
+        authenticator.verify(token)
+        
+        const id = req.params.id
+
+        const userDataBase = new UserDatabase()
+        const user = await userDataBase.getUserById(id)       
+
+        res.status(200).send({
+            id: user.id,
+            email: user.email
+        })
+        
+    } catch(err){
+        res.status(400).send({
+            message: err.message
+        })
+    }
+})
 ```
 
 <br><br>
--- semana17/aula55_Autenticacao.no.Backend-Criptografia.e.User.Roles/exercicios-tarde/src/index.ts
+++ semana17/aula55_Autenticacao.no.Backend-Criptografia.e.User.Roles/exercicios-tarde/src/index.ts
@@ -314,6 +314,35 @@ app.delete("/user/:id", async(req: Request, res: Response) => {
 
 
 // ====================================================================
+// =============================== 6 ==================================
+// ====================================================================
+
+app.get("/user/:id", async(req: Request, res: Response) => {
+    try{
+        const token = req.headers.authorization as string
+
+        const authenticator = new Authenticator()
+        authenticator.verify(token)
+        
+        const id = req.params.id
+
+        const userDataBase = new UserDatabase()
+        const user = await userDataBase.getUserById(id)       
+
+        res.status(200).send({
+            id: user.id,
+            email: user.email
+        })
+        
+    } catch(err){
+        res.status(400).send({
+            message: err.message
+        })
+    }
+})
+
+
+// ====================================================================
 
 
 const server = app.listen(process.env.PORT || 3000, () => {
