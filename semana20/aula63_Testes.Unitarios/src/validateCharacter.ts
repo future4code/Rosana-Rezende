7ed53cea023cdf096ab0add2973eafc00119702b
Rosana Rezende <rezende_rosana@hotmail.com>
2020-06-02 13:06:17 -0300

    exercício 1

---
+++ semana20/aula63_Testes.Unitarios/src/validateCharacter.ts
@@ -0,0 +1,28 @@
+export function validateCharacter(input: Character): boolean {
+    if(
+        !input.name ||
+        input.life === undefined ||
+        input.defense === undefined ||
+        input.force === undefined
+    ){
+        return false
+    }
+
+    if(
+        input.life < 0 ||
+        input.defense < 0 ||
+        input.force < 0
+    ){
+        return false
+    }
+
+    return true
+    
+}
+
+interface Character {
+    name: string,
+    life: number,
+    defense: number,
+    force: number
+}
